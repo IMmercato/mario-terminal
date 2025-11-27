816da72d92feaa7773504641407bd9b881802c88
IMmercato <imesh.fernando100@gmail.com>
2025-11-27 19:36:40 +0100

    server setup

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'mario')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'mario', 'mario.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+    console.log("SuperMario")
+});
